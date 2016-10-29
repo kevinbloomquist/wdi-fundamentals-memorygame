dc871e4dc03de034194acb090774f7fcca10d21b
kevinbloomquist <rkevinbloom@gmail.com>
2016-10-29 19:54:47 -0400

    Add files via upload
    ch 9 commit

---
+++ main.js
@@ -0,0 +1,27 @@
+// initial game logic//
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+if (cardTwo===cardFour) {
+	alert("Match!");
+}else if (cardTwo!==cardFour) {
+	alert( "Sorry try again")
+}else if (cardOne!==cardTwo) {
+	alert( "Sorry try again")
+}
+
+if (cardOne===cardTwo) {
+	alert("Match!");
+}else if (cardOne!==cardTwo) {
+	alert( "Sorry try again")
+}
+
+if (cardOne===cardTwo) {
+	alert("Match!");
+}else if (cardOne!==cardTwo) {
+	alert( "Sorry try again")
+}
+console.log("JS file is connected to HTML! Woo!")
