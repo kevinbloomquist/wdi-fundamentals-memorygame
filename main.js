2be28ef7ad81d7cdf78c5cb67eb28cd7b4bb21c4
kevinbloomquist <rkevinbloom@gmail.com>
2016-10-29 20:39:43 -0400

    Add files via upload
    simplified if else...

--- main.js
+++ main.js
@@ -5,23 +5,12 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardTwo===cardFour) {
-	alert("Match!");
-}else if (cardTwo!==cardFour) {
-	alert( "Sorry try again")
-}else if (cardOne!==cardTwo) {
-	alert( "Sorry try again")
+if (cardOne === cardTwo) {
+	alert('You found a match!');	
+} else if (cardThree===cardFour) {
+	alert('You found a match!');
+} else {
+  alert('Sorry, try again.');
 }
 
-if (cardOne===cardTwo) {
-	alert("Match!");
-}else if (cardOne!==cardTwo) {
-	alert( "Sorry try again")
-}
-
-if (cardOne===cardTwo) {
-	alert("Match!");
-}else if (cardOne!==cardTwo) {
-	alert( "Sorry try again")
-}
-console.log("JS file is connected to HTML! Woo!")
+console.log("JS file is connected to HTML! Woo!");
