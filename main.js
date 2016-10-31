9bfca2435876b67ab4106edb452831e42891c2f1
kevinbloomquist <rkevinbloom@gmail.com>
2016-10-30 21:56:19 -0600

    Add files via upload
    lost cards, not sure?

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+/*From ch:9
+
 if (cardOne === cardTwo) {
 	alert('You found a match!');	
 } else if (cardThree===cardFour) {
@@ -13,4 +15,17 @@ if (cardOne === cardTwo) {
   alert('Sorry, try again.');
 }
 
-console.log("JS file is connected to HTML! Woo!");
+console.log("JS file is connected to HTML! Woo!");*/
+//find the board...
+var board = document.getElementById('game-board');
+
+//make DOM board...
+function createBoard() {
+//card loop
+for (var i=0; i<cards.length; i++) {
+	var cardElement = document.createElement('div');
+	cardElement.className = 'card';
+	board.appendChild(cardElement);
+}
+};
+
